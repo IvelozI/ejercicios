c8951b8e8f18bb793378f90570c22ca373932de3
Veloz <alexisfabian.barria@alumnos.ulagos.cl>
2024-09-10 19:56:09 -0300

    ejemplos

---
+++ Heroes.js
@@ -0,0 +1,17 @@
+function teamCap() {
+    var heroe = "Capitan America"; 
+    //variable heroe con el valor Capitan America
+    console.log(heroe); 
+    // Se imprime el valor de heroe 
+}
+
+teamCap(); // Se llama a la función teamCap, lo que ejecuta el console.log y muestra Capitan America
+
+function teamIronMan() {
+    var heroe = "Iron Man"; 
+    // variable heroe con el valor "Iron Man"
+    console.log(heroe); 
+    // Se imprime el valor de heroe 
+}
+
+teamIronMan(); // Se llama a la función teamIronMan, lo que ejecuta el console.log y muestra Capitan America
---
+++ Xmen.js
@@ -0,0 +1,12 @@
+var comic = "X-Men"; 
+// Variable almacena el nombre del comic
+
+function comprar() {
+    console.log(`Compraste el comic ${comic}`); 
+    // Imprime compraste el comic
+}
+
+function leer() {
+    console.log(`Estás leyendo el comic ${comic}`); 
+    // Imprime estás leyendo el comic
+}
